aa21f3e1e0af811320b0984933a8c9c5a2c630c1
Berkut Ksu <76900160+BerkutKsu@users.noreply.github.com>
2026-03-23 20:30:47 +0200

    Update task1.go

--- task1.go
+++ task1.go
@@ -11,5 +11,29 @@
 
 
 func topKFrequentElements(nums []int, k int) []int {
-  
+  uniq := make(map[int]int)
+	for _, num := range nums {
+		uniq[num]++
+	}
+	amountUniq := len(uniq)
+
+	sortArray := make([][]int, amountUniq)
+	i := 0
+	for key, val := range uniq {
+		sortArray[i] = []int{key, val}
+		i++
+	}
+
+	for i := 0; i < amountUniq-1; i++ {
+		for j := i + 1; j < amountUniq; j++ {
+			if sortArray[i][1] < sortArray[j][1] {
+				sortArray[i], sortArray[j] = sortArray[j], sortArray[i]
+			}
+		}
+	}
+	var result []int
+	for i = 0; i < k; i++ {
+		result = append(result, sortArray[i][0])
+	}
+	return result
 }
