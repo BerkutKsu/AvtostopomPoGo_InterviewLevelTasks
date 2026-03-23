900b0bbab23d1dd31ca7ea8baf17503577ffa26d
Berkut Ksu <76900160+BerkutKsu@users.noreply.github.com>
2026-03-23 20:40:10 +0200

    Update task11.go

--- task11.go
+++ task11.go
@@ -1,4 +1,26 @@
 //Написать код функции, которая делает merge N каналов.
 //Весь входной поток перенаправляется в один канал.
 
-func joinChannels(chs ...<-chan int) <-chan int {}
+func joinChannels(chs ...<-chan int) <-chan int {
+  var wg sync.WaitGroup
+  out := make(chan int)
+	
+	forward := func(ch <-chan int) {
+		defer wg.Done()
+		for v := range ch {
+			out <- v
+		}
+	}
+	wg.Add(len(chs))
+
+	for _, ch := range chs {
+		go forward(ch)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
+}
