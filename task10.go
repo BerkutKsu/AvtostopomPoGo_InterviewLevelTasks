65d3da3bbab6f839f87888a56170b4d413bb50db
Berkut Ksu <76900160+BerkutKsu@users.noreply.github.com>
2026-03-24 13:04:25 +0200

    Update task10.go

--- task10.go
+++ task10.go
@@ -3,3 +3,50 @@
 //то нужно закрыть все остальные каналы
 
 func case3(channels ...chan int) chan int {
+ out := make(chan int)
+	done := make(chan struct{})
+
+	var wg sync.WaitGroup
+	var closeOnce sync.Once
+	var closeDoneOnce sync.Once
+
+	forward := func(ch <-chan int) {
+		defer wg.Done()
+		for v := range ch {
+			select {
+			case out <- v:
+			case <-done:
+				return
+			}
+		}
+		closeDoneOnce.Do(func() {
+			close(done)
+		})
+	}
+
+	go func() {
+		for _, ch := range channels {
+			wg.Add(1)
+			go forward(ch)
+		}
+
+		wg.Wait()
+		close(out)
+	}()
+
+	go func() {
+		<-done
+		closeOnce.Do(func() {
+			for _, ch := range channels {
+				if ch != nil {
+					func() {
+						defer func() { recover() }()
+						close(ch)
+					}()
+				}
+			}
+		})
+	}()
+
+	return out 
+}
